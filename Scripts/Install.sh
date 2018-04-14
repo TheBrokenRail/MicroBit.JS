4b16d8ff47a47ee569f19abeedbd9ff7401533ec
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-04-14 19:49:19 -0400

    Update Install.sh

--- Scripts/Install.sh
+++ Scripts/Install.sh
@@ -7,7 +7,7 @@ wget http://duktape.org/duktape-2.2.0.tar.xz
 tar xvfJ duktape-2.2.0.tar.xz
 cd duktape-2.2.0
 rm -rf src-custom
-python tools/configure.py --output-directory Build/Duktape
+python tools/configure.py --output-directory ../Build/Duktape
 cp -a Build/Duktape/. ./
 cd ../
 
