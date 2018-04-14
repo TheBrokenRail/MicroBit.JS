a19a60aacb89ba589a407e96f8d19d1e7cb4f599
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-04-14 13:40:03 -0400

    Create Install.sh

---
+++ Scripts/Install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+mkdir Build
+
+wget -O bootstrap.py https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py && python bootstrap.py
+git clone --depth 1 https://github.com/mozilla/gecko-dev.git
+
+cd gecko-dev/js/src
+autoconf-2.13
+
+mkdir ../Build/SpiderMonkey
+cd ../Build/SpiderMonkey
+../../gecko-dev/js/src/configure
+make
+
+cd ../../
+pip install yotta
