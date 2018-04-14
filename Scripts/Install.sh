49893f619e43126c8a3f1a37c366944de3d8d949
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-04-14 19:11:10 -0400

    Update Install.sh

--- Scripts/Install.sh
+++ Scripts/Install.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
+wget -O http://duktape.org/duktape-2.2.0.tar.xz
+tar xvfJ duktape-2.2.0.tar.xz
+cd duktape-2.2.0
+rm -rf src-custom
+python tools/configure.py
+cp -a src-custom/. ./
+cd ../
+
 mkdir Build
 
-wget -O bootstrap.py https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py && printf "1\n1\n1\n2\n" | python bootstrap.py
-git clone --depth 1 https://github.com/mozilla/gecko-dev.git
-
-cd gecko-dev/js/src
-autoconf
-
-mkdir ../../../Build/SpiderMonkey
-cd ../../../Build/SpiderMonkey
-../../gecko-dev/js/src/configure
-make
-
-cd ../../
-pip install yotta
+sudo pip install yotta
 yotta target bbc-microbit-classic-gcc
