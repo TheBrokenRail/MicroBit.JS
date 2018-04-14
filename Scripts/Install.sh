a138c5985611d7045b17124395df109261b6edca
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-04-14 15:10:31 -0400

    Update Install.sh

--- Scripts/Install.sh
+++ Scripts/Install.sh
@@ -2,14 +2,14 @@
 
 mkdir Build
 
-wget -O bootstrap.py https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py && echo '1' | python bootstrap.py
+wget -O bootstrap.py https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py && echo '11' | python bootstrap.py
 git clone --depth 1 https://github.com/mozilla/gecko-dev.git
 
 cd gecko-dev/js/src
 autoconf-2.13
 
-mkdir ../Build/SpiderMonkey
-cd ../Build/SpiderMonkey
+mkdir ../../../Build/SpiderMonkey
+cd ../../../Build/SpiderMonkey
 ../../gecko-dev/js/src/configure
 make
 
