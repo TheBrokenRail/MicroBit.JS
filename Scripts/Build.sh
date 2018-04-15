325cb8e6fc285e7b895d6bcaa4839654af637bca
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-04-14 20:00:42 -0400

    Update and rename Install.sh to Build.sh

--- Scripts/Build.sh
+++ Scripts/Build.sh
@@ -8,8 +8,10 @@ tar xvfJ duktape-2.2.0.tar.xz
 cd duktape-2.2.0
 rm -rf src-custom
 python tools/configure.py --output-directory ../Build/Duktape
-cp -a Build/Duktape/. ./
+cp -a Build/Duktape/. ./source
 cd ../
 
 sudo pip install yotta
 yotta target bbc-microbit-classic-gcc
+yotta build
+cp -a build/bbc-microbit-classic-gcc/source/. Build
