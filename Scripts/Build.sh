5554cc94d6ebbbe15d33be1e6676f4db7f88c34d
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-04-15 12:34:16 -0400

    Update Build.sh

--- Scripts/Build.sh
+++ Scripts/Build.sh
@@ -6,7 +6,7 @@ mkdir Build
 mkdir Source/include
 
 git clone --depth=1 https://github.com/cesanta/mjs.git
-cp mjs/. Source/include
+cp -r mjs/. Source/include
 
 sudo pip install yotta
 yotta target bbc-microbit-classic-gcc
