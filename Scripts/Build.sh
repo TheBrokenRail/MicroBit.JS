383ef7eed675edbf35869593776c395c3d4bfc1f
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-04-15 16:53:50 -0400

    Update Build.sh

--- Scripts/Build.sh
+++ Scripts/Build.sh
@@ -11,7 +11,7 @@ cp mjs/mjs.h Source/include
 
 sudo pip install yotta
 yotta target bbc-microbit-classic-gcc
-echo $CFLAGS
-export CFLAGS=-fpermissive
+echo $CXXFLAGS
+export CXXFLAGS=-fpermissive
 yotta build
 cp build/bbc-microbit-classic-gcc/Source/MicroBit-JS-combined.hex Build
