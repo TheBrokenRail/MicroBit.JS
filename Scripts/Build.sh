eb0339789f8a9b4e58b69b1cc331e08ec78cba68
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-04-14 22:54:41 -0400

    Update Build.sh

--- Scripts/Build.sh
+++ Scripts/Build.sh
@@ -3,17 +3,15 @@
 set -e
 
 mkdir Build
-mkdir Build/Duktape
 
 wget http://duktape.org/duktape-2.2.0.tar.xz
 tar xvfJ duktape-2.2.0.tar.xz
 cd duktape-2.2.0
 rm -rf src-custom
-python tools/configure.py --output-directory ../Build/Duktape
-cp -r ../Build/Duktape/. ./Source
+python tools/configure.py --output-directory ../Source
 cd ../
 
 sudo pip install yotta
 yotta target bbc-microbit-classic-gcc
 yotta build
-cp -r build/bbc-microbit-classic-gcc/Source/. Build
+cp build/bbc-microbit-classic-gcc/Source/MicroBit.JS-combined.hex Build
