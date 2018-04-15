b562b3427e60ad66f55dd3e5c941e16924e4d775
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-04-15 12:28:18 -0400

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,17 +1,26 @@
 #include "MicroBit.h"
-#include "include/duktape.h"
+#include "include/mjs.h"
+#include <cstring>
 
 MicroBit uBit;
+
+void displayScroll(char x) {
+  uBit.display.scroll(x);
+}
+
+void *getFFI(void *handle, const char *name) {
+  if (strcmp(name, "displayScroll") == 0) return displayScroll;
+  return NULL;
+}
 
 int main()
 {
     // Initialise the micro:bit runtime.
     uBit.init();
 
-    duk_context* ctx = duk_create_heap_default();
-    duk_eval_string(ctx, "'test'");
-    duk_pop(ctx);
-    duk_destroy_heap(ctx);
+    struct mjs *mjs = mjs_create();
+    mjs_set_ffi_resolver(mjs, getFFI);
+    mjs_exec(mjs, "let micro = {}; micro.displayScroll = ffi('void displayScroll(char)'); micro.displayScroll('1234')", NULL);
 
     // If main exits, there may still be other fibers running or registered event handlers etc.
     // Simply release this fiber, which will mean we enter the scheduler. Worse case, we then
