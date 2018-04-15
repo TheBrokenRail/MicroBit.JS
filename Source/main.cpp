4398cad0a5b169a77a44230118153faca26c970d
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-04-14 23:25:55 -0400

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -10,6 +10,8 @@ int main()
 
     duk_context* ctx = duk_create_heap_default();
     duk_destroy_heap(ctx);
+    duk_eval_string(ctx, "'test'");
+    duk_pop(ctx);
 
     // If main exits, there may still be other fibers running or registered event handlers etc.
     // Simply release this fiber, which will mean we enter the scheduler. Worse case, we then
