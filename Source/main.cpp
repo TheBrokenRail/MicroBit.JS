a8408624a8d63f3229675d3b85e35e036b660cef
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-04-14 23:08:48 -0400

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,4 +1,5 @@
 #include "MicroBit.h"
+#include <dukglue/dukglue.h>
 
 MicroBit uBit;
 
@@ -7,8 +8,8 @@ int main()
     // Initialise the micro:bit runtime.
     uBit.init();
 
-    // Insert your code here!
-    uBit.display.scroll("HELLO WORLD! :)");
+    duk_context* ctx = duk_create_heap_default();
+    duk_destroy_heap(ctx);
 
     // If main exits, there may still be other fibers running or registered event handlers etc.
     // Simply release this fiber, which will mean we enter the scheduler. Worse case, we then
