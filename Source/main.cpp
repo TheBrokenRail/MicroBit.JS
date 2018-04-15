a4ce3eee38bf9b4e31a4ed6b96d55ee77b25aca4
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-04-14 23:26:23 -0400

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -9,9 +9,9 @@ int main()
     uBit.init();
 
     duk_context* ctx = duk_create_heap_default();
-    duk_destroy_heap(ctx);
     duk_eval_string(ctx, "'test'");
     duk_pop(ctx);
+    duk_destroy_heap(ctx);
 
     // If main exits, there may still be other fibers running or registered event handlers etc.
     // Simply release this fiber, which will mean we enter the scheduler. Worse case, we then
