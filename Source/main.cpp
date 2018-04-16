33fac3d5071a2a271843ae3c0c76da3d573308ea
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-04-16 10:16:18 -0400

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -9,9 +9,16 @@ void displayScroll(char *x) {
   uBit.display.scroll(x);
 }
 
+void sleep(int x) {
+  uBit.sleep(x);
+}
+
 void *ffiResolver(void *handle, const char *name) {
   if (strcmp(name, "displayScroll") == 0) {
     return (void *)displayScroll;
+  }
+  if (strcmp(name, "sleep") == 0) {
+    return (void *)sleep;
   }
   return NULL;
 }
@@ -22,7 +29,7 @@ int main() {
 
     struct mjs *mjs = mjs_create();
     mjs_set_ffi_resolver(mjs, ffiResolver);
-    char str[] = "let micro = {}; micro.displayScroll = ffi('void displayScroll(char *)'); ";
+    char str[] = "let uBit = {}; uBit.sleep = ffi('void sleep(int)'); uBit.display = {}; uBit.display.scroll = ffi('void displayScroll(char *)'); ";
     mjs_exec(mjs, strcat(str, jsSource.c_str()), NULL);
 
     // If main exits, there may still be other fibers running or registered event handlers etc.
