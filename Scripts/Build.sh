5e7d670ee2320f5ef981e390d059c6a9643febff
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-04-18 14:23:45 -0400

    Update Build.sh

--- Scripts/Build.sh
+++ Scripts/Build.sh
@@ -12,11 +12,15 @@ cp mjs/mjs.h Source
 
 tput setaf 3; tput bold; echo "Installing Yotta"
 pip install --user yotta
-tput setaf 3; tput bold; echo "Setting Yotta Target"
-yotta target bbc-microbit-classic-gcc
 tput setaf 3; tput bold; echo "Creating Build Directory"
 mkdir Build
-tput setaf 3; tput bold; echo "Building Project"
+tput setaf 3; tput bold; echo "Setting Yotta Target for BBC MicroBit"
+yotta target bbc-microbit-classic-gcc
+tput setaf 3; tput bold; echo "Building Project for BBC MicroBit"
 yotta build
-tput setaf 3; tput bold; echo "Copying Output to GH-Pages"
-cp build/bbc-microbit-classic-gcc/Source/microbit-js-combined.hex Build/MicroBit.JS.hex
+tput setaf 3; tput bold; echo "Setting Yotta Target for Linux"
+yotta target x86-linux-native
+tput setaf 3; tput bold; echo "Building Project for BBC Linux"
+yotta build
+tput setaf 3; tput bold; echo "Copying Output"
+cp -r build/* Build
