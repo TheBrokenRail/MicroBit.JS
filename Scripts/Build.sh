9446348de41893217a9be56dd376abc6dadf38ba
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-04-18 12:19:20 -0400

    Update Build.sh

--- Scripts/Build.sh
+++ Scripts/Build.sh
@@ -2,22 +2,21 @@
 
 set -e
 
-echo "Generating C++ Source for JS File"
+tput setaf 3; tput bold; echo "Generating C++ Source for JS File"
 printf '#include <string>\nstd::string jsSource = R"~~~~('"$(cat Source/main.js)"')~~~~";' > Source/JSSource.h
 
-mkdir Build
-mkdir Source/include
-
-echo "Cloning mJS"
+tput setaf 3; tput bold; echo "Cloning mJS"
 git clone --depth=1 https://github.com/cesanta/mjs.git
-cp mjs/mjs.c Source/include
-cp mjs/mjs.h Source/include
+cp mjs/mjs.c Source
+cp mjs/mjs.h Source
 
-echo "Installing Yotta"
+tput setaf 3; tput bold; echo "Installing Yotta"
 pip install --user yotta
-echo "Setting Yotta Target"
+tput setaf 3; tput bold; echo "Setting Yotta Target"
 yotta target bbc-microbit-classic-gcc
-echo "Building Project"
+tput setaf 3; tput bold; echo "Creating Build Directory"
+mkdir Build
+tput setaf 3; tput bold; echo "Building Project"
 yotta build
-echo "Copying Output to GH-Pages"
+tput setaf 3; tput bold; echo "Copying Output to GH-Pages"
 cp build/bbc-microbit-classic-gcc/Source/MicroBit-JS-combined.hex Build
