966041ff406934da9706368ff1377618a28b9fa1
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-04-19 19:42:36 -0400

    Update main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -131,9 +131,12 @@ int main() {
   uBit.serial.printf("FFI, ");
   mjs_set_ffi_resolver(mjsObj, ffiResolver);
   uBit.serial.printf("JS, '");
-  uBit.serial.printf(strcat((char *)initJS.c_str(), jsSource.c_str()));
+  char *jsStr = "";
+  jsStr.append(initJS);
+  jsStr.append(jsSource);
+  uBit.serial.printf(jsStr);
   uBit.serial.printf("', Execute, ");
-  mjs_err_t err = mjs_exec(mjsObj, strcat((char *)initJS.c_str(), jsSource.c_str()), NULL);
+  mjs_err_t err = mjs_exec(mjsObj, jsStr, NULL);
   if (err) {
     const char *errStr = mjs_strerror(mjsObj, err);
     uBit.serial.printf(errStr);
