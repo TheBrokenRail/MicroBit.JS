384c588f9a599ee6061281a8913cfe4fb4ff425f
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-05-05 09:24:10 -0400

    Update Build.sh

--- Scripts/Build.sh
+++ Scripts/Build.sh
@@ -10,6 +10,28 @@ git clone --depth=1 https://github.com/cesanta/mjs.git
 cp mjs/mjs.c Source
 cp mjs/mjs.h Source
 
+mkdir Lib
+git clone https://github.com/jerryscript-project/jerryscript.git
+cd jerryscript
+mkdir build
+EXT_CFLAGS=-D__TARGET_MBED_BBC-MICROBIT-CLASSIC-GCC -mlittle-endian -mthumb -mcpu=cortex-m4 -Wno-error=format=
+cmake -Bbuild -H./ \
+  -DCMAKE_SYSTEM_NAME=Mbed \
+	-DCMAKE_SYSTEM_PROCESSOR=armv7l-hf \
+  -DCMAKE_C_COMPILER=arm-none-eabi-gcc \
+  -DCMAKE_C_COMPILER_WORKS=TRUE \
+  -DENABLE_LTO=OFF \
+  -DENABLE_ALL_IN_ONE=OFF \
+  -DJERRY_LIBC=OFF \
+  -DJERRY_CMDLINE=OFF \
+  -DEXTERNAL_COMPILE_FLAGS="$EXT_CFLAGS" \
+  -DMEM_HEAP_SIZE_KB=16
+make -C$(BUILD_DIR) jerry-core
+make -C$(BUILD_DIR) jerry-libm
+cp build/lib/. ../Lib
+cd ../
+dir Lib
+
 tput setaf 3; tput bold; echo "Installing Yotta"
 pip install --user yotta
 tput setaf 3; tput bold; echo "Setting Yotta Target"
