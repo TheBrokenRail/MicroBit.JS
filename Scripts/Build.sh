ac9b2208f56f7757e0169c17de152c45a14d7726
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-05-06 09:37:44 -0400

    Update Build.sh

--- Scripts/Build.sh
+++ Scripts/Build.sh
@@ -10,16 +10,19 @@ git clone --depth=1 https://github.com/cesanta/mjs.git
 cp mjs/mjs.c Source
 cp mjs/mjs.h Source
 
+tput setaf 3; tput bold; echo "Installing Yotta"
+pip install --user yotta
+tput setaf 3; tput bold; echo "Setting Yotta Target"
+yotta target bbc-microbit-classic-gcc
+
+yotta install
 mkdir Lib
 git clone https://github.com/jerryscript-project/jerryscript.git
 cd jerryscript
 mkdir build
 EXT_CFLAGS='-D__TARGET_MBED_BBC-MICROBIT-CLASSIC-GCC'
 cmake -Bbuild -H./ \
-  -DCMAKE_SYSTEM_NAME=Mbed \
-  -DCMAKE_SYSTEM_PROCESSOR=armv7l-hf \
-  -DCMAKE_C_COMPILER=arm-none-eabi-gcc \
-  -DCMAKE_C_COMPILER_WORKS=TRUE \
+  -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake \
   -DENABLE_LTO=OFF \
   -DENABLE_ALL_IN_ONE=OFF \
   -DJERRY_LIBC=OFF \
@@ -31,10 +34,6 @@ make -Cbuild jerry-libm
 cp -r build/lib/. ../Lib
 cd ../
 
-tput setaf 3; tput bold; echo "Installing Yotta"
-pip install --user yotta
-tput setaf 3; tput bold; echo "Setting Yotta Target"
-yotta target bbc-microbit-classic-gcc
 tput setaf 3; tput bold; echo "Building Project"
 VERBOSE=1 yotta build
 tput setaf 3; tput bold; echo "Copying Output"
