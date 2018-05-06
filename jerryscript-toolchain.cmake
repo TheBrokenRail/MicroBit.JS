6f1ed24f819c3f5b196b37233421f8f16a547aea
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-05-06 10:28:23 -0400

    Update and rename toolchain.cmake to jerryscript-toolchain.cmake

--- jerryscript-toolchain.cmake
+++ jerryscript-toolchain.cmake
@@ -1,2 +1,3 @@
 include("${CMAKE_CURRENT_LIST_DIR}/yotta_targets/mbed-gcc/CMake/toolchain.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/yotta_targets/bbc-microbit-classic-gcc/CMake/toolchain.cmake")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-implicit-fallthrough")
