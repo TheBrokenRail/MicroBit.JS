e69320d3ff3424477ad84a03a6ccc7418e85cce8
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-05-06 19:20:07 -0400

    Delete jerryscript-toolchain.cmake

--- jerryscript-toolchain.cmake
+++
@@ -1,3 +0,0 @@
-include("${CMAKE_CURRENT_LIST_DIR}/yotta_targets/mbed-gcc/CMake/toolchain.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/yotta_targets/bbc-microbit-classic-gcc/CMake/toolchain.cmake")
-set(EXTERNAL_COMPILE_FLAGS "${EXTERNAL_COMPILE_FLAGS} -Wno-error=implicit-fallthrough")
